114eda965f93c31fbb47d398e7f33d06eb2bc6d9
phycomob50-commits <phycomob50@gmail.com>
2026-07-24 01:17:57 +0700

    Add admin dashboard

---
+++ admin/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import User from "./testdashboard/user";
+import Product from "./testdashboard/product";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<User />} />
+      <Route path="/product" element={<Product />} />
+      <Route path="/" element={<User />} />
+      <Route path="/user" element={<User />} />
+      <Route path="/product" element={<Product />} />
+    </Routes>
+  );
+}
+
+export default App;
